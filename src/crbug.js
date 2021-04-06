c0cf2f4bdcceeae57a3779d86dde49b465d82b70
Samuel Attard <sattard@slack-corp.com>
2021-04-05 22:51:37 -0700

    feat: add support for crbug.com URLs

---
+++ src/crbug.js
@@ -0,0 +1,135 @@
+const fetch = require('node-fetch').default;
+
+const { Policy, ConstantBackoff } = require('cockatiel');
+
+function parseBugIdentifier(url) {
+  const parsed = new URL(url);
+  if (parsed.host === 'bugs.chromium.org') {
+    // https://bugs.chromium.org/p/chromium/issues/detail?id=1195924
+    const number = parseInt(parsed.searchParams.get('id'), 10);
+    if (isNaN(number)) return null;
+
+    const match = /^https:\/\/bugs\.chromium\.org\/p\/([a-z0-9]+)\/issues\/detail/g.exec(url);
+    if (!match) return null;
+
+    return {
+      project: match[1],
+      number,
+    }
+  } else if (parsed.host === 'crbug.com') {
+    // https://crbug.com/12345
+    const number = parseInt(parsed.pathname.slice(1), 10);
+    if (isNaN(number)) return null;
+
+    return {
+      project: 'chromium',
+      number,
+    };
+  }
+
+  return null;
+}
+
+const retry = Policy.handleAll()
+  .retry()
+  .attempts(3)
+  .backoff(new ConstantBackoff(250));
+
+async function getMonorailToken() {
+  return retry.execute(async () => {
+    const url = 'https://bugs.chromium.org/p/chromium';
+    const result = await fetch(url);
+    const text = await result.text();
+    const rgxResult = /.*'token': '([\w\d_-]*)',.*/gi.exec(text);
+
+    if (!rgxResult) {
+      throw new Error(`Could not find token in ${url}`);
+    }
+
+    return rgxResult[1];
+  });
+}
+
+async function getMonorailHeaders() {
+  return {
+    "accept": "application/json",
+    "content-type": "application/json",
+    "x-xsrf-token": await getMonorailToken()
+  }
+}
+
+module.exports = async function handleChromiumBugUnfurl(url, message_ts, channel) {
+  const bugIdentifier = parseBugIdentifier(url);
+  if (!bugIdentifier) return false;
+
+  const headers = await getMonorailHeaders();
+  const monorailQuery = JSON.stringify({
+    issueRef: {
+      localId: bugIdentifier.number,
+      projectName: bugIdentifier.project,
+    },
+  });
+  const response = fetch('https://bugs.chromium.org/prpc/monorail.Issues/GetIssue', {
+    method: 'POST',
+    headers,
+    body: monorailQuery,
+  });
+  const commentResponse = fetch('https://bugs.chromium.org/prpc/monorail.Issues/ListComments', {
+    method: 'POST',
+    headers,
+    body: monorailQuery
+  })
+  if ((await response).status !== 200) return false;
+  if ((await commentResponse).status !== 200) return false;
+
+  const { issue } = JSON.parse((await (await response).text()).slice(4));
+  const { comments } = JSON.parse((await (await commentResponse).text()).slice(4));
+  
+  const unfurl = await fetch('https://slack.com/api/chat.unfurl', {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${process.env.SLACK_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      channel,
+      ts: message_ts,
+      unfurls: {
+        [url]: {
+          color: issue.statusRef.meansOpen ? '#36B37E' : '#FF5630',
+          author_name: issue.reporterRef.displayName,
+          // author_icon: owner.avatars && owner.avatars.length ? owner.avatars[owner.avatars.length - 1].url : ':void',
+          author_link: `https://bugs.chromium.org/u/${issue.reporterRef.userId}/`,
+          fallback: `[${issue.projectName}] #${issue.localId} ${issue.summary}`,
+          title: `#${issue.localId} ${issue.summary}`,
+          title_link: url,
+          footer_icon: 'https://bugs.chromium.org/static/images/monorail.ico',
+          text: comments[0].content,
+          footer: `<https://bugs.chromium.org/p/${issue.projectName}|crbug/${issue.projectName}>`,
+          ts: issue.openedTimestamp * 1000,
+          fields: [issue.componentRefs && issue.componentRefs.length ? {
+            title: 'Components',
+            value: issue.componentRefs.map(ref => `• <https://bugs.chromium.org/p/${issue.projectName}/issues/list?q=component%3A${encodeURIComponent(ref.path)}|\`${ref.path.replace(/>/g, '→')}\`>`).join('\n'),
+            short: true,
+          } : null, issue.labelRefs && issue.labelRefs.length ? {
+            title: 'Labels',
+            value: issue.labelRefs.map(ref => `• <https://bugs.chromium.org/p/${issue.projectName}/issues/list?q=label%3A${encodeURIComponent(ref.label)}|\`${ref.label}\`>`).join('\n'),
+            short: true,
+          } : null, {
+            title: 'Comments',
+            value: `${comments.length}`,
+            short: true,
+          }].filter(Boolean)
+        }
+      }
+    })
+  });
+  if (unfurl.status === 200) {
+    const resp = await unfurl.json();
+    if (!resp.ok) {
+      console.error('Failed to unfurl', resp);
+    }
+  }
+  
+  return true;
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const fetch = require('node-fetch').default;
+
+const handleChromiumReviewUnfurl = require('./chromium-review.js');
+const handleChromiumBugUnfurl = require('./crbug');
 
 const app = express();
 app.use(require('body-parser').json());
@@ -28,57 +30,6 @@ async function handleChromiumLink(teamId, event, token) {
   if (!links || !links.length) return;
 
   const url = links[0].url;
-  const match = /^https:\/\/chromium-review\.googlesource\.com\/c\/([a-z0-9]+)\/([a-z0-9]+)\/\+\/([0-9]+)/g.exec(url);
-  if (match) {
-    const repo = `${match[1]}%2F${match[2]}`;
-    const niceRepo = `${match[1]}/${match[2]}`;
-    const cl = parseInt(match[3], 10);
-
-    const detailsUrl = `https://chromium-review.googlesource.com/changes/${repo}~${cl}/detail?O=916314`;
-    const detailsResponse = await fetch(detailsUrl);
-    const detailsText = await detailsResponse.text();
-    const details = JSON.parse(detailsText.substr(4));
-    const { project, subject, owner, labels, current_revision, revisions } = details;
-    const commit = revisions[current_revision].commit;
-    const { message, author: { date } } = commit;
-    const messageWithoutSubject = message.startsWith(subject) ? message.substr(subject.length + 1).trim() : message;
-
-    const unfurl = await fetch('https://slack.com/api/chat.unfurl', {
-      method: 'POST',
-      headers: {
-        'Authorization': `Bearer ${process.env.SLACK_TOKEN}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        // token: '',
-        channel,
-        ts: message_ts,
-        unfurls: {
-          [url]: {
-            color: '#4D394B',
-            author_name: owner.name,
-            author_icon: owner.avatars && owner.avatars.length ? owner.avatars[owner.avatars.length - 1].url : ':void',
-            author_link: `https://chromium-review.googlesource.com/q/author:${encodeURIComponent(owner.email)}`,
-            fallback: `[${niceRepo}] #${cl} ${subject}`,
-            title: `#${cl} ${subject}`,
-            title_link: url,
-            footer_icon: 'https://chromium-review.googlesource.com/favicon.ico',
-            text: messageWithoutSubject,
-            footer: `<https://source.chromium.org/chromium/${niceRepo}|${niceRepo}>`,
-            ts: (new Date(date)).getTime(),
-            // TODO: Labels? CQ status?
-            // fields: [{
-              
-            // }]
-          }
-        }
-      })
-    });
-    if (unfurl.status === 200) {
-      const resp = await unfurl.json();
-      if (!resp.ok) {
-        console.error('Failed to unfurl', resp);
-      }
-    }
-  }
+  if (await handleChromiumReviewUnfurl(url, message_ts, channel)) return;
+  if (await handleChromiumBugUnfurl(url, message_ts, channel)) return;
 }

662a8341f0c3bb2d0cf15c7013da605172c87288
Samuel Attard <marshallofsound@electronjs.org>
2023-02-06 12:52:28 -0800

    fix: use markdown syntax for bold

--- src/escape.ts
+++ src/escape.ts
@@ -1,6 +1,11 @@
 export const escapeSlackMessage = (text: string): string => {
   // Naively attempt to replace bold tags with Slack syntax
-  text = text.replace(/<b>/g, '**').replace(/<\/b>/g, '**');
+  text = text.replace(/<b>/g, '*').replace(/<\/b>/g, '*');
+  // Some stack traces include "*.mm" which is a recognized domain in slack
+  // but we don't want to linkify
+  text = text.replace(/([a-z])\.mm([^a-z]|$)/g, '$1&period;mm$2');
   // Escape all magic Slack Characters
-  return text.replace(/\&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+  text = text.replace(/\&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+
+  return text;
 };

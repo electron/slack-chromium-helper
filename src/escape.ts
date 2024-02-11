88ccd7e95d66141acdb4da9f14315420ab97daf9
Samuel Attard <marshallofsound@electronjs.org>
2024-02-11 01:27:39 -0800

    fix: handle excessive new lines

--- src/escape.ts
+++ src/escape.ts
@@ -3,6 +3,8 @@ export const escapeSlackMessage = (text: string): string => {
   text = text.replace(/<b>/g, '*').replace(/<\/b>/g, '*');
   // Try and replace markdown bold syntax with slack bold syntax
   text = text.replace(/\*\*([^\*]+)\*\*/g, '*$1*');
+  // Sometimes we end up with excessive newlines after a conversion, let's strip em down to 2
+  text = text.replace(/(?: +\n){3,}/gm, '\n\n');
   // Some stack traces include "*.mm" which is a recognized domain in slack
   // but we don't want to linkify
   text = text.replace(/([a-z])\.mm([^a-z]|$)/g, '$1&period;mm$2');
